d73038064a1531a87502b0d725fb3edba58778e2
aftm1614 <abalverde1694@gmail.com>
2024-06-11 16:38:38 +0000

    bringup: droidx

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_topaz.mk
+    $(LOCAL_DIR)/droidx_topaz.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_topaz-user \
-    lineage_topaz-userdebug \
-    lineage_topaz-eng
+    droidx_topaz-user \
+    droidx_topaz-userdebug \
+    droidx_topaz-eng
