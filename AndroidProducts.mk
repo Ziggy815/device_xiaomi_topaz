a8a2ea615e3f5cf3f6912ae2d4242c2cec2cee6c
aftm1614 <abalverde1694@gmail.com>
2024-06-11 01:32:26 +0000

    bringup: StatixOS!

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_topaz.mk
+    $(LOCAL_DIR)/statix_topaz.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_topaz-user \
-    lineage_topaz-userdebug \
-    lineage_topaz-eng
+    statix_topaz-user \
+    statix_topaz-userdebug \
+    statix_topaz-eng
