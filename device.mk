bc0a329daa607f1b89f1f002a89abc86560e1db9
AspectPower <95100392+AspectPower@users.noreply.github.com>
2024-06-14 15:49:22 +0300

    topaz: Speed profile services and wifi-service to reduce RAM and storage
    Change-Id: I1d907b24cb2760ddd9cc65ac4335730c8e61a0a3
    Signed-off-by: NikitaSavc <micraftur@gmail.com>

--- device.mk
+++ device.mk
@@ -36,9 +36,9 @@ PRODUCT_PACKAGES += \
     libsuspend \
     charger_res_images
 
-# Dex/ART optimization
+# Dex/ART optimization Speed profile services and wifi-service to reduce RAM and storage.
 PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
-PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := everything
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 USE_DEX2OAT_DEBUG := false
 
 # Overlays
