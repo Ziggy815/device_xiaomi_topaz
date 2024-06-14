08eb1a1cef6fc6cf265f0994f52e9dd0ef44f72d
AspectPower <95100392+AspectPower@users.noreply.github.com>
2024-06-14 15:53:18 +0300

    topaz: Bring Back PREOPT_DEFAULT_COMPILER_FILTER settings
     * use everything filter for dexpreopt (AOSPA/android_vendor_pa@ab5061c)
    
    Bringing it back.

--- device.mk
+++ device.mk
@@ -38,6 +38,7 @@ PRODUCT_PACKAGES += \
 
 # Dex/ART optimization Speed profile services and wifi-service to reduce RAM and storage.
 PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
+PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := everything
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 USE_DEX2OAT_DEBUG := false
 
